3cf10ebb843e923eae781e645c687fd53b094ea5
Shoaib Sulehri <shoaibrajput294@gmail.com>
2019-03-16 17:10:20 +0500

    home pge done

---
+++ Final Year Project/Controllers/FirstController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Final_Year_Project.Controllers
+{
+    public class FirstController : Controller
+    {
+        public IActionResult FirstPage()
+        {
+            return View();
+        }
+    }
+}
