fce8e3b0fb2cab22ec201b9b89b58f571a2b501e
Shoaib Sulehri <shoaibrajput294@gmail.com>
2019-03-27 11:01:59 +0500

    Admin Changes

---
+++ Final Year Project/Controllers/AdminController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Final_Year_Project.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult AdminPage()
+        {
+            return View();
+        }
+
+        //Login Page
+        public IActionResult LoginPage()
+        {
+            return View();
+        }
+
+        //Registration Page
+        public IActionResult RegisterPage()
+        {
+            return View();
+        }
+    }
+}
